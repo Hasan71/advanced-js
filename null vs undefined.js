b797c5dc71b60626b0278245f9e3db08cbf7614f
Hasan71 <mahmud.cse7@gmail.com>
2020-03-04 00:26:47 +0600

    adding null vs undefined

---
+++ null vs undefined.js
@@ -0,0 +1,25 @@
+//let premika;
+
+
+function myCode(num1, num2){
+    //console.log(num1+num2);
+    return num1+num2
+}
+
+//let add = myCode(10);
+
+console.log(myCode(10, 20));
+
+
+let myObject = {id: 1615, name: "hasan", phone:016}
+
+console.log(myObject.name);
+
+let ami = undefined;
+
+console.log(ami);
+
+let tumi = null;
+
+console.log(tumi);
+
